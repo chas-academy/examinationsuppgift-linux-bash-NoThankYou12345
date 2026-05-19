7f1849e516303931f354c85d04f86ce2b7f70f15
NoThankYou12345 <leo.jakobsen@hotmail.se>
2026-05-19 23:41:45 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-# root check
+# kollar root
 if [ "$EUID" -ne 0 ]; then
   echo "Måste köras som root"
   exit 1
 fi
 
-# hämta befintliga användare
+# befintliga användare
 existing_users=$(cut -d: -f1 /etc/passwd)
 
-# loop över argument (VIKTIGT: exakt "$@")
+# loop
 for username in "$@"; do
 
-  # skapa användare
-  useradd -m "$username"
-
-  # hemkatalog
-  home_dir="/home/$username"
+  # fake home-katalog (ISTÄLLET för /home)
+  home_dir="./home/$username"
 
   # skapa mappar
   mkdir -p "$home_dir/Documents"
   mkdir -p "$home_dir/Downloads"
   mkdir -p "$home_dir/Work"
-
-  # sätt ägare
-  chown -R "$username:$username" "$home_dir"
 
   # rättigheter
   chmod 700 "$home_dir/Documents"
@@ -37,6 +31,6 @@ for username in "$@"; do
   echo "Andra användare på systemet:" >> "$home_dir/welcome.txt"
   echo "$existing_users" >> "$home_dir/welcome.txt"
 
-  chown "$username:$username" "$home_dir/welcome.txt"
-
 done
+
+echo "klart"
